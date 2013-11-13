78f6e4c4ac54524380efccedeb4799eb37844187
Tai Hu <hutai66@gmail.com>
2013-11-13 14:50:17 -0500

    Initial check in

---
+++ SourceTableUI/src/com/sourcetable/ui/SourceTableMainWindow.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sourcetable.ui;
+
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Tai Hu
+ */
+public class SourceTableMainWindow extends Stage {
+}
