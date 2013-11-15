b25cc178465ff7db9a5617d3fd04cb33c39cf5f0
Tai Hu <hutai66@gmail.com>
2013-11-15 17:39:30 -0500

    Implementing main window

--- SourceTableUI/src/com/sourcetable/ui/SourceTableMainWindow.java
+++ SourceTableUI/src/com/sourcetable/ui/SourceTableMainWindow.java
@@ -4,11 +4,72 @@
  */
 package com.sourcetable.ui;
 
-import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
 
 /**
  *
  * @author Tai Hu
  */
-public class SourceTableMainWindow extends Stage {
+public class SourceTableMainWindow extends BorderPane {
+    public SourceTableMainWindow() {
+        buildUI();
+    }
+    
+    private void buildUI() {
+        setId("rootPane");
+        
+        setLeft(getLeftPane());
+        setCenter(getCenterPane());
+    }
+    
+    private Pane getLeftPane() {
+        GridPane leftPane = new GridPane();
+        RowConstraints rc = new RowConstraints();
+        rc.setPercentHeight(100);
+        leftPane.getRowConstraints().add(rc);
+        
+        Pane navButtonPane = getNavButtonPane();
+        leftPane.getColumnConstraints().add(new ColumnConstraints(40));
+        leftPane.add(navButtonPane, 0, 0);
+        
+        Pane navigationPane = getNavigationPane();
+        leftPane.getColumnConstraints().add(new ColumnConstraints(250));
+        leftPane.add(navigationPane, 1, 0);
+        
+        return leftPane;
+    }
+    
+    private Pane getCenterPane() {
+        BorderPane centerPane = new BorderPane();
+        
+        return centerPane;
+    }
+    
+    private Pane getNavButtonPane() {
+        VBox navButtonPane = new VBox();
+        navButtonPane.setId("navButtonPane");
+        
+        Button datasourceNavPaneButton = new Button();
+        datasourceNavPaneButton.setId("datasourceNavPaneButton");
+        Button datasourceObjButton = new Button();
+        datasourceObjButton.setId("datasourceObjButton");
+        
+        navButtonPane.getChildren().addAll(datasourceNavPaneButton, datasourceObjButton);
+        
+        return navButtonPane;
+    }
+    
+    private Pane getNavigationPane() {
+        VBox navigationPane = new VBox();
+        navigationPane.setId("navigationPane");
+        
+        return navigationPane;
+    }
 }
--- SourceTableUI/src/com/sourcetable/ui/SourceTableUI.java
+++ SourceTableUI/src/com/sourcetable/ui/SourceTableUI.java
@@ -5,12 +5,10 @@
 package com.sourcetable.ui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import static com.sourcetable.ui.common.UIConstants.*;
 
 /**
  *
@@ -20,34 +18,24 @@ public class SourceTableUI extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        SourceTableMainWindow mainWindow = new SourceTableMainWindow();        
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        // Set up stage
+//        primaryStage.initStyle(StageStyle.UNDECORATED);
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        Scene scene = new Scene(mainWindow, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
+        scene.getStylesheets().add(SourceTableUI.class.getResource("SourceTableUI.css").toExternalForm());
+                
+        primaryStage.setTitle("Source Table");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.err.println("Warning: This main method should not be used. Check JavaFX configuration.");
         launch(args);
     }
 }
