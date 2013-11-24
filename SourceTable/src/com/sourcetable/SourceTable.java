3666e024f4814ba7573976d2efc71aad2445860f
yulinwen <yulinwen@yulinwen-PC>
2013-11-24 21:35:07 +0800

    Working on generating SQL.

--- SourceTable/src/com/sourcetable/SourceTable.java
+++ SourceTable/src/com/sourcetable/SourceTable.java
@@ -11,6 +11,7 @@ import com.sourcetable.metadata.*;
 import com.sourcetable.session.*;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -18,14 +19,24 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.sql.generation.api.grammar.booleans.BooleanExpression;
+import org.sql.generation.api.grammar.builders.query.ColumnsBuilder;
+import org.sql.generation.api.grammar.builders.query.QuerySpecificationBuilder;
+import org.sql.generation.api.grammar.builders.query.TableReferenceBuilder;
+import org.sql.generation.api.grammar.common.SetQuantifier;
 import org.sql.generation.api.vendor.MySQLVendor;
 import org.sql.generation.api.vendor.SQLVendor;
 import org.sql.generation.api.vendor.SQLVendorProvider;
 import org.sql.generation.api.grammar.factories.BooleanFactory;
 import org.sql.generation.api.grammar.factories.ColumnsFactory;
 import org.sql.generation.api.grammar.factories.LiteralFactory;
+import org.sql.generation.api.grammar.factories.QueryFactory;
 import org.sql.generation.api.grammar.factories.TableReferenceFactory;
+import org.sql.generation.api.grammar.query.ColumnReference;
+import org.sql.generation.api.grammar.query.ColumnReferenceByName;
+import org.sql.generation.api.grammar.query.Ordering;
 import org.sql.generation.api.grammar.query.QueryExpression;
+import org.sql.generation.api.grammar.query.QueryExpressionBody;
 
 /**
  *
@@ -237,7 +248,9 @@ public class SourceTable {
         } else {
             System.out.println("Not found...");
         }
+        System.out.println("------------------------------");
         
+        System.out.println("Dumping sql...");
         // start generating sql
         // Create or acquire vendor
         SQLVendor vendor = null;
@@ -253,7 +266,6 @@ public class SourceTable {
           FROM table
           WHERE table.value = 5
           ORDER BY 1 ASC
-        */
         
         BooleanFactory b = vendor.getBooleanFactory();
         ColumnsFactory c = vendor.getColumnsFactory();
@@ -271,5 +283,140 @@ public class SourceTable {
         // String sqlString = vendor.toString( query );
         String sqlString2 = query.toString();
         System.out.println("Output sql is: " + sqlString2);
+        */
+        
+        // @formatter:off
+        /*
+          SELECT t0.entity_identity
+          FROM (SELECT DISTINCT t0.entity_pk, t0.entity_identity
+            FROM qi4j.entities t0
+            WHERE t0.entity_type_id IN (3, 4)
+            EXCEPT
+            SELECT DISTINCT t0.entity_pk, t0.entity_identity
+            FROM qi4j.entities t0
+            JOIN qi4j.qname_6 t1 ON (t0.entity_pk = t1.entity_pk AND t1.parent_qname IS NULL)
+            JOIN qi4j.qname_11 t2 ON (t1.qname_id = t2.parent_qname AND t1.entity_pk = t2.entity_pk)
+            LEFT JOIN qi4j.qname_12 t3 ON (t2.qname_id = t3.parent_qname AND t2.entity_pk = t3.entity_pk)
+            LEFT JOIN qi4j.qname_13 t4 ON (t2.qname_id = t4.parent_qname AND t2.entity_pk = t4.entity_pk)
+            WHERE t0.entity_type_id IN (3, 4)
+            GROUP BY t0.entity_pk, t0.entity_identity
+            HAVING COUNT(t2.qname_value) >= 2
+            ORDER BY t0.entity_pk ASC
+          ) AS t0
+        */
+        // @formatter:on
+
+        QueryFactory q = vendor.getQueryFactory();
+        BooleanFactory b = vendor.getBooleanFactory();
+        TableReferenceFactory t = vendor.getTableReferenceFactory();
+        LiteralFactory l = vendor.getLiteralFactory();
+        ColumnsFactory c = vendor.getColumnsFactory();
+
+        QuerySpecificationBuilder firstInnerQuery = q.querySpecificationBuilder();
+        
+        // construct select
+        // get all expressions from all attributes
+        ArrayList<ColumnReferenceByName> colRefByName = new ArrayList();
+        HashMap attributes = userSession.getAttributes();
+        Iterator it = attributes.entrySet().iterator();
+        while (it.hasNext()) {            
+            Map.Entry pairs = (Map.Entry)it.next();
+            
+            AttributeMeta anAttr = (AttributeMeta) pairs.getValue();
+            ArrayList<ExpressionMeta> exps = anAttr.getExpressions();
+            for (int i = 0; i < exps.size(); i++) {
+                ColumnReferenceByName aColExp = c.colName(exps.get(i).getExpression());
+                colRefByName.add(aColExp);
+            }
+        }
+        ColumnReference[] colRefAttr = new ColumnReference[colRefByName.size()];
+        colRefAttr = colRefByName.toArray(colRefAttr);                
+        ColumnsBuilder innerSelectCols = q.columnsBuilder().addUnnamedColumns(colRefAttr);
+
+        // get all expressions from all metrics
+        colRefByName = new ArrayList();
+        HashMap metrics = userSession.getMetrics();
+        it = metrics.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pairs = (Map.Entry)it.next();
+            
+            MetricMeta anMet = (MetricMeta) pairs.getValue();
+            ArrayList<ExpressionMeta> exps = anMet.getExpressions();
+            for (int i = 0; i < exps.size(); i++) {
+                ColumnReferenceByName aColExp = c.colName(exps.get(i).getExpression());
+                colRefByName.add(aColExp);
+            }
+        }
+        ColumnReference[] colRefMet = new ColumnReference[colRefByName.size()];
+        colRefMet = colRefByName.toArray(colRefMet);
+        innerSelectCols.addUnnamedColumns(colRefMet);
+        
+        // construct groupby
+        //QuerySpecificationBuilder specBuilder = q.querySpecificationBuilder();
+        if ((colRefAttr.length > 0) && (colRefMet.length > 0)) {
+            firstInnerQuery.getGroupBy().addGroupingElements(q.groupingElement(colRefAttr));
+        }
+
+        // construct join
+        
+        
+        
+        
+        
+        ColumnReferenceByName innerFirstCol = c.colName( "t0", "entity_pk" );
+        ColumnReferenceByName innerSecondCol = c.colName( "t0", "entity_identity" );
+        // ColumnsBuilder innerSelectCols = q.columnsBuilder().addUnnamedColumns(innerFirstCol, innerSecondCol );                                                        
+        
+        BooleanExpression where = b.in( c.colName( "t0", "entity_type_id" ), l.n( 3 ), l.n( 4 ) );
+        firstInnerQuery.setSelect( innerSelectCols );                              
+        firstInnerQuery.getFrom().addTableReferences(
+            t.tableBuilder( t.table( t.tableName( "qi4j", "entities" ), t.tableAlias( "t0" ) ) ) );
+
+        firstInnerQuery.getWhere().reset( where );
+
+        TableReferenceBuilder join = t
+            .tableBuilder( t.table( t.tableName( "qi4j", "entities" ), t.tableAlias( "t0" ) ) )
+            .addQualifiedJoin(
+                org.sql.generation.api.grammar.query.joins.JoinType.INNER,
+                t.table( t.tableName( "qi4j", "qname_6" ), t.tableAlias( "t1" ) ),
+                t.jc( b.booleanBuilder( b.eq( c.colName( "t0", "entity_pk" ), c.colName( "t1", "entity_pk" ) ) )
+                    .and( b.isNull( c.colName( "t1", "parent_qname" ) ) ).createExpression() ) )
+            .addQualifiedJoin(
+                org.sql.generation.api.grammar.query.joins.JoinType.INNER,
+                t.table( t.tableName( "qi4j", "qname_11" ), t.tableAlias( "t2" ) ),
+                t.jc( b.booleanBuilder( b.eq( c.colName( "t1", "qname_id" ), c.colName( "t2", "parent_qname" ) ) )
+                    .and( b.eq( c.colName( "t1", "entity_pk" ), c.colName( "t2", "entity_pk" ) ) ).createExpression() ) )
+            .addQualifiedJoin(
+                org.sql.generation.api.grammar.query.joins.JoinType.LEFT_OUTER,
+                t.table( t.tableName( "qi4j", "qname_12" ), t.tableAlias( "t3" ) ),
+                t.jc( b.booleanBuilder( b.eq( c.colName( "t2", "qname_id" ), c.colName( "t3", "parent_qname" ) ) )
+                    .and( b.eq( c.colName( "t2", "entity_pk" ), c.colName( "t3", "entity_pk" ) ) ).createExpression() ) )
+            .addQualifiedJoin(
+                org.sql.generation.api.grammar.query.joins.JoinType.LEFT_OUTER,
+                t.table( t.tableName( "qi4j", "qname_13" ), t.tableAlias( "t4" ) ),
+                t.jc( b.booleanBuilder( b.eq( c.colName( "t3", "qname_id" ), c.colName( "t4", "parent_qname" ) ) )
+                    .and( b.eq( c.colName( "t2", "entity_pk" ), c.colName( "t4", "entity_pk" ) ) ).createExpression() ) );
+
+        QuerySpecificationBuilder secondBuilder = q.querySpecificationBuilder();
+        secondBuilder.setSelect( innerSelectCols );
+        secondBuilder.getFrom().addTableReferences( join );
+        secondBuilder.getWhere().reset( where );
+        secondBuilder.getGroupBy().addGroupingElements( q.groupingElement( innerFirstCol ),
+            q.groupingElement( innerSecondCol ) );
+        secondBuilder.getHaving().reset( b.geq( l.func( "COUNT", c.colName( "t2", "qname_value" ) ), l.n( 2 ) ) );
+        secondBuilder.getOrderBy().addSortSpecs( q.sortSpec( c.colName( "t0", "entity_pk" ), Ordering.ASCENDING ) );
+
+        QueryExpressionBody innerQuery = q.queryBuilder( firstInnerQuery.createExpression() )
+            .except( secondBuilder.createExpression() ).createExpression();
+
+        QuerySpecificationBuilder select = q.querySpecificationBuilder().setSelect(
+            q.columnsBuilder().addUnnamedColumns( c.colName( "t0", "entity_identity" ) ) );
+        select.getFrom().addTableReferences(
+            t.tableBuilder( t.table( q.createQuery( innerQuery ), t.tableAlias( "t0" ) ) ) );
+
+        QueryExpression query = q.createQuery( q.queryBuilder( select.createExpression() ).createExpression() );
+
+        String sqlString = query.toString();
+        System.out.println("Output sql is: " + sqlString);       
     }    
 }
