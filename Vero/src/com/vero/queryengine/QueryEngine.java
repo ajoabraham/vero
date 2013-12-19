b60c281768dec46f3eb254642508a1d69d363fe5
yulinwen <yulinwen@yulinwen-T420i.office.in>
2013-12-19 14:45:10 +0800

    Work on queryengine/stage.

--- Vero/src/com/vero/queryengine/QueryEngine.java
+++ Vero/src/com/vero/queryengine/QueryEngine.java
@@ -9,6 +9,8 @@ package com.vero.queryengine;
 import com.vero.metadata.Attribute;
 import com.vero.metadata.JoinDefinition;
 import com.vero.session.Session;
+import java.util.Iterator;
+import java.util.Set;
 import org.jgrapht.alg.KruskalMinimumSpanningTree;
 import org.jgrapht.graph.ClassBasedEdgeFactory;
 import org.jgrapht.graph.DefaultWeightedEdge;
@@ -33,22 +35,40 @@ public class QueryEngine {
         public void setJoinDef(JoinDefinition inJoinDef) {
             joinDef = inJoinDef;
         }
+        
+        @Override
+        public double getWeight() {
+            return super.getWeight();
+        }
     }
         
     private Stage stage;
     private WeightedMultigraph<Attribute, EdgeUnit> joinGraph;
-    private WeightedMultigraph<String, EdgeUnit> testGraph;
     
     public QueryEngine() {
         stage = new Stage();
-        joinGraph = new WeightedMultigraph(new ClassBasedEdgeFactory<Attribute, EdgeUnit>(EdgeUnit.class));
-        //testGraph = new WeightedMultigraph(new ClassBasedEdgeFactory<String, EdgeUnit>(EdgeUnit.class));        
+        joinGraph = new WeightedMultigraph(new ClassBasedEdgeFactory<Attribute, EdgeUnit>(EdgeUnit.class));                
     }
             
     public void preprocess(Session inSession) {
         stage.preprocess(inSession);
+
+        //expirimentOnGraph();
+        // loop on attributes/metrics
         
-        /*
+        // for each expression, find tables used and connect vertex and create edges
+        
+        // loop each vertex and remove edges that have same definition until one left        
+        
+        // mst algo
+        //KruskalMinimumSpanningTree kmt = new KruskalMinimumSpanningTree(joinGraph);
+        //System.out.println("kmt total cost: " + kmt.getMinimumSpanningTreeTotalWeight());        
+    }
+    
+    private void expirimentOnGraph() {
+        WeightedMultigraph<String, EdgeUnit> testGraph
+            = new WeightedMultigraph(new ClassBasedEdgeFactory<String, EdgeUnit>(EdgeUnit.class));
+        
         testGraph.addVertex("V1");
         testGraph.addVertex("V2");
         testGraph.addVertex("V3");
@@ -73,15 +93,13 @@ public class QueryEngine {
         EdgeUnit e5 = new EdgeUnit();
         testGraph.setEdgeWeight(e5, 4);
         testGraph.addEdge("V4", "V3", e5);
-        */
-        
-        // loop on attributes/metrics
-        
-        // for each expression, find tables used and connect vertex and create edges
-        
-        // mst algo
-        KruskalMinimumSpanningTree kmt = new KruskalMinimumSpanningTree(joinGraph);
+        KruskalMinimumSpanningTree kmt = new KruskalMinimumSpanningTree(testGraph);
+        Set kmtEdges = kmt.getMinimumSpanningTreeEdgeSet();
+        Iterator<EdgeUnit> kmtIter = kmtEdges.iterator();
+        while (kmtIter.hasNext()) {
+            EdgeUnit eU = kmtIter.next();
+            System.out.println("Vertext: " + eU +":"+"cost: " + eU.getWeight());
+        }
         System.out.println("kmt total cost: " + kmt.getMinimumSpanningTreeTotalWeight());
-        
     }
 }
