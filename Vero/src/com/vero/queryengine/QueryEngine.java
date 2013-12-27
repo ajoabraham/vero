7b24afe15f4f077fd4b97e504fb032195dffab54
yulinwen <yulinwen@yulinwen-T420i.office.in>
2013-12-27 10:23:41 +0800

    Work on queryengine/grapht.

--- Vero/src/com/vero/queryengine/QueryEngine.java
+++ Vero/src/com/vero/queryengine/QueryEngine.java
@@ -75,7 +75,7 @@ public class QueryEngine {
         // for each table, find where it is used and connect vertex and create edges
         for (Map.Entry<UUID, ProcessingUnit> entry : puMap.entrySet()) {
             ProcessingUnit pu = entry.getValue();
-            System.out.println("### Current PU = " + pu);
+            System.out.println("### Current PU content = " + pu.getContent());
             ArrayList<Table> listTables = pu.retrieveTables();
             if (listTables.size() > 0) {
                 Iterator<Table> iterTable = listTables.iterator();
@@ -84,70 +84,77 @@ public class QueryEngine {
                     Table aTab = iterTable.next();
                     System.out.println("Processing table: " + aTab.getPhysicalName());
                     HashMap<String, JoinDefinition> tabJD = stage.getJoindefHTByTable(aTab.getPhysicalName());
-                    HashMap<String, JoinDefinition> allTabJDs = tabJD;
-                    Map<String, JoinDefinition> allJDMap = allTabJDs;
-                    for (Map.Entry<String, JoinDefinition> jdEntry : allJDMap.entrySet()) {
-                        JoinDefinition curJD = jdEntry.getValue();
-                        String otherTable = curJD.getOtherTable(aTab.getPhysicalName());
-                        System.out.println("curJD = " + curJD.getName() + ", other table = " + otherTable);                                                
-                        
-                        HashMap<String, Attribute> otherAttrHT = stage.getAttributeHTByTable(otherTable);
-                        HashMap<String, Attribute> allOtherAttrs = otherAttrHT;
-                        Map<String, Attribute> allOtherAttrsMap = allOtherAttrs;
-                        for (Map.Entry<String, Attribute> allOtherAttrsEntry : allOtherAttrsMap.entrySet()) {
-                            Attribute otherAttr = allOtherAttrsEntry.getValue();
-                            
-                            System.out.println("Other Attr: " + otherAttr.getName());
-                            Map<UUID, ProcessingUnit> OtherPUMap = allPUs;
-                            for (Map.Entry<UUID, ProcessingUnit> otherPUEntry : OtherPUMap.entrySet()) {
-                                ProcessingUnit otherPU = otherPUEntry.getValue();
-                                
-                                if (otherPU.getContent() == otherAttr) {
-                                    System.out.println("Found PU == otherAttr");
-                                    int rowCost = aTab.getRowCount();
-                                    int otherRowCost = inSession.getTable(otherTable).getRowCount();
-                                    double weight = (double)rowCost * (double)otherRowCost;
-                                    System.out.println("RowCost = " + rowCost + ", otherRowCost = " + otherRowCost + ", weight = " + weight);
-                                    
-                                    EdgeUnit aEU = new EdgeUnit();
-                                    aEU.setJoinDef(curJD);
-                                    joinGraph.setEdgeWeight(aEU, weight);
-                                    joinGraph.addEdge(pu, otherPU, aEU);
+
+                    if (tabJD != null) {                    
+                        HashMap<String, JoinDefinition> allTabJDs = tabJD;
+                        Map<String, JoinDefinition> allJDMap = allTabJDs;
+                        for (Map.Entry<String, JoinDefinition> jdEntry : allJDMap.entrySet()) {
+                            JoinDefinition curJD = jdEntry.getValue();
+                            String otherTable = curJD.getOtherTable(aTab.getPhysicalName());
+                            System.out.println("curJD = " + curJD.getName() + ", other table = " + otherTable);                                                
+
+                            HashMap<String, Attribute> otherAttrHT = stage.getAttributeHTByTable(otherTable);
+                            HashMap<String, Attribute> allOtherAttrs = otherAttrHT;
+                            Map<String, Attribute> allOtherAttrsMap = allOtherAttrs;
+                            for (Map.Entry<String, Attribute> allOtherAttrsEntry : allOtherAttrsMap.entrySet()) {
+                                Attribute otherAttr = allOtherAttrsEntry.getValue();
+
+                                System.out.println("Other Attr: " + otherAttr.getName());
+                                Map<UUID, ProcessingUnit> OtherPUMap = allPUs;
+                                for (Map.Entry<UUID, ProcessingUnit> otherPUEntry : OtherPUMap.entrySet()) {
+                                    ProcessingUnit otherPU = otherPUEntry.getValue();
+
+                                    if (otherPU.getContent() == otherAttr) {
+                                        System.out.println("Found PU == otherAttr");
+                                        int rowCost = aTab.getRowCount();
+                                        int otherRowCost = inSession.getTable(otherTable).getRowCount();
+                                        double weight = (double)rowCost * (double)otherRowCost;
+                                        System.out.println("RowCost = " + rowCost + ", otherRowCost = " + otherRowCost + ", weight = " + weight);
+
+                                        EdgeUnit aEU = new EdgeUnit();
+                                        aEU.setJoinDef(curJD);
+                                        joinGraph.setEdgeWeight(aEU, weight);
+                                        joinGraph.addEdge(pu, otherPU, aEU);
+                                    }
                                 }
-                            }
-                        }                                                
-                        
-                        HashMap<String, Metric> otherMetricHT = stage.getMetricHTByTable(otherTable);
-                        HashMap<String, Metric> allOtherMetrics = otherMetricHT;
-                        Map<String, Metric> allOtherMetricsMap = allOtherMetrics;
-                        for (Map.Entry<String, Metric> allOtherMetricsEntry : allOtherMetricsMap.entrySet()) {
-                            Metric otherMetric = allOtherMetricsEntry.getValue();
-                            
-                            System.out.println("Other Metric: " + otherMetric.getName());
-                            Map<UUID, ProcessingUnit> OtherPUMap = allPUs;
-                            for (Map.Entry<UUID, ProcessingUnit> otherPUEntry : OtherPUMap.entrySet()) {
-                                ProcessingUnit otherPU = otherPUEntry.getValue();
-                                
-                                if (otherPU.getContent() == otherMetric) {
-                                    System.out.println("Found PU == otherMetric");
-                                    int rowCost = aTab.getRowCount();
-                                    int otherRowCost = inSession.getTable(otherTable).getRowCount();
-                                    double weight = (double)rowCost * (double)otherRowCost;
-                                    System.out.println("RowCost = " + rowCost + ", otherRowCost = " + otherRowCost + ", weight = " + weight);
-                                    
-                                    EdgeUnit aEU = new EdgeUnit();
-                                    aEU.setJoinDef(curJD);
-                                    joinGraph.setEdgeWeight(aEU, weight);
-                                    joinGraph.addEdge(pu, otherPU, aEU);
-                                }
-                            }                            
-                        }                       
+                            }                                                
+
+                            HashMap<String, Metric> otherMetricHT = stage.getMetricHTByTable(otherTable);
+                            HashMap<String, Metric> allOtherMetrics = otherMetricHT;
+                            Map<String, Metric> allOtherMetricsMap = allOtherMetrics;
+                            for (Map.Entry<String, Metric> allOtherMetricsEntry : allOtherMetricsMap.entrySet()) {
+                                Metric otherMetric = allOtherMetricsEntry.getValue();
+
+                                System.out.println("Other Metric: " + otherMetric.getName());
+                                Map<UUID, ProcessingUnit> OtherPUMap = allPUs;
+                                for (Map.Entry<UUID, ProcessingUnit> otherPUEntry : OtherPUMap.entrySet()) {
+                                    ProcessingUnit otherPU = otherPUEntry.getValue();
+
+                                    if (otherPU.getContent() == otherMetric) {
+                                        System.out.println("Found PU == otherMetric");
+                                        int rowCost = aTab.getRowCount();
+                                        int otherRowCost = inSession.getTable(otherTable).getRowCount();
+                                        double weight = (double)rowCost * (double)otherRowCost;
+                                        System.out.println("RowCost = " + rowCost + ", otherRowCost = " + otherRowCost + ", weight = " + weight);
+
+                                        EdgeUnit aEU = new EdgeUnit();
+                                        aEU.setJoinDef(curJD);
+                                        joinGraph.setEdgeWeight(aEU, weight);
+                                        joinGraph.addEdge(pu, otherPU, aEU);
+                                    }
+                                }                            
+                            }                       
+                        }
                     }
                 }
             }
         }
         
         // loop each vertex and remove edges that have same definition until one left        
+        
+        
+        // dump graph
+        dumpGraph(joinGraph);
         
         // mst algo
         KruskalMinimumSpanningTree kmt = new KruskalMinimumSpanningTree(joinGraph);
@@ -157,7 +164,25 @@ public class QueryEngine {
             System.out.println("Edge joindef name = " + eu.getJoinDef().getName() + ", weight = " + eu.getWeight());
         }
     }
-        
+    
+    private void dumpGraph(WeightedMultigraph<ProcessingUnit, EdgeUnit> inGraph) {
+        System.out.println("### Dumping graph...");
+        Set<ProcessingUnit> graphVertexSet = inGraph.vertexSet();
+        int vertexCount = 0;
+        for (ProcessingUnit pu : graphVertexSet) {
+            System.out.println("  Vertex = " + vertexCount + " : " + pu.getContent());
+            Set<EdgeUnit> graphEdgeSet = inGraph.edgesOf(pu);
+            
+            int edgeCount = 0;
+            for (EdgeUnit eu : graphEdgeSet) {
+                System.out.println("    Edge = " + edgeCount + " : " + eu.joinDef.getName());
+                edgeCount++;
+            }
+            
+            vertexCount++;
+        }
+    }
+    
     private void expirimentOnGraph() {
         WeightedMultigraph<String, EdgeUnit> testGraph
             = new WeightedMultigraph(new ClassBasedEdgeFactory<String, EdgeUnit>(EdgeUnit.class));
