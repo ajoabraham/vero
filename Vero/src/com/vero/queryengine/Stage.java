91d29031e69d8e53fcd2ebcd344fd975ba4770bd
yulinwen <yulinwen@yulinwen-PC>
2014-01-13 22:14:33 +0800

    Adding test4.

--- Vero/src/com/vero/queryengine/Stage.java
+++ Vero/src/com/vero/queryengine/Stage.java
@@ -14,7 +14,6 @@ import com.vero.session.Session;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
